b2498a036794866fd042b4df7e885e7b0d048d7f
Marcelo <marcelo@MacBook-Pro-de-Marcelo.local>
2022-06-29 08:18:16 -0300

    Criado o admin do Sistema

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route'
+import User from 'App/Models/User'
 
 Route.get('/', async () => {
-  return { hello: 'world' }
-})
+  User.create({
+    email: 'admin@mail.com',
+    password: '123456',
+    tipo: 'admin'
+  });
+});
